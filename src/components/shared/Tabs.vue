de230a432f05b8a361cc19f3458fffc5341cd451
skvorsovoleg <o.skvortsov@logema.org>
2025-01-10 16:31:58 +0300

    Был сбой в папках из-за этого гит не понимал какие файлы брать

---
+++ src/components/shared/Tabs.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import TabTrigger from "../ui/TabTrigger.vue";
+
+defineProps<{
+  items: Array
+}>();
+
+const id = ref(1);
+
+const handleTab = (item) => {
+  id.value = item.id;
+}
+
+</script>
+
+<template>
+  <ul class = "tabs">
+    <li
+        v-for="item in items"
+        :key="item.id"
+    >
+      <TabTrigger @click-tab="handleTab(item)" :active = "id === item.id">{{item.text}}</TabTrigger>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+  .tabs{
+    list-style-type: none;
+    display: flex;
+    gap: 20px;
+    flex-wrap: wrap;
+    max-width: 435px;
+  }
+</style>
